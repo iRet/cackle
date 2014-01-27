205a03eba6d4a0d74db2d9a45a2538e9e9039c2d
artyom <artyom@TurboSnail.local>
2014-01-27 18:23:01 +0800

    default_scope fixed

--- lib/app/models/cackle_comment.rb
+++ lib/app/models/cackle_comment.rb
@@ -1,5 +1,5 @@
 class CackleComment < ActiveRecord::Base
   self.primary_key = 'comment_id'
-  default_scope order: 'created_at DESC'
+  default_scope       { order('created_at DESC') }
   scope :approved, -> { where(status: 'approved') }
 end
